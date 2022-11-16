3b8a9ceead0833f830b85c6a92f82117f958018c
rlucas <lucas.reboulet@ynov.com>
2022-11-16 10:01:00 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func main() {
+
+	Handlefunc()
+}
+func Handlefunc() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		template := template.Must(template.ParseFiles("Page/index.html"))
+		if r.Method != http.MethodPost {
+			template.Execute(w, "")
+			return
+		}
+	})
+	fs := http.FileServer(http.Dir("Static/"))
+	http.Handle("/Static/", http.StripPrefix("/Static/", fs))
+	fmt.Println("http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
